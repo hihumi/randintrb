2c2dc537e9ccfa643eb6a682dd50ee9761015968
hihumi <yui.maa5800@gmail.com>
2016-12-28 07:26:30 +0900

    fix

--- rand_int.rb
+++ rand_int.rb
@@ -1,23 +1,29 @@
 #!/usr/bin/env ruby
 
 class RandInt
-  def rand_int()
-    puts "任意の整数を入力してください"
-    puts "その整数の個数分の乱数を出力します"
-    puts "まず配列として出力され、つぎに改行して出力されます"
+  def rand_int
+    puts "--- 乱数生成プログラム ---"
+    puts "任意の正の整数を入力してください(1000以下)"
+    puts "乱数が0からその整数nまで、その整数の個数分、配列として出力されます"
     print "> "
     res1 = gets
+
     res2 = res1.to_i
-    res3 = Array.new(res2) { rand res2 }
-    puts "配列として出力:"
-    res_array = res3.to_ary
-    print res_array
-    puts
-    puts
-    puts "改行して出力:"
-    puts res3
+    if res2 > 1000
+      puts "大きすぎる整数です"
+    elsif res2 < 0
+      puts "正の整数を入力してください"
+    elsif res2 == 0
+      puts "0、または不正な入力です"
+    else
+      res3 = Array.new(res2) { rand res2 }
+      puts "出力:"
+      print res3
+      puts
+    end
+
   end
 end
 
 ri = RandInt.new
-ri.rand_int()
+ri.rand_int
